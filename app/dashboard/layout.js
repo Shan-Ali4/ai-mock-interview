fe8e2bd441ee078d63d642b78ebfe748e7d0ba63
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2026-06-17 01:26:59 +0530

    update layout

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const DashboardLayout = ({}) => {
+const DashboardLayout = ({chidlren}) => {
   return (
     <div>
-      Dashboard Layout
+      {chidlren}
     </div>
   )
 }
